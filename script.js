aab785b4d05ab51334735bcd9fcebc5806fc6ad4
Flynevsky <wowsohio@gmail.com>
2022-11-23 20:44:15 +0100

    V 1.0

---
+++ script.js
@@ -0,0 +1,9 @@
+function onmouseover0(id) {
+    let case_choix = document.querySelector("#" + id);
+    case_choix.style.transform= "scale(1.1)";
+}
+function onmouseout0(id) {
+    let case_choix = document.querySelector("#" + id);
+    case_choix.style.transform= "scale(1.0)";
+
+} 
